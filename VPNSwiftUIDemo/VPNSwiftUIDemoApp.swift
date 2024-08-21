99aa072d764f11e37f9411ace829d71c47686909
Dev <developer.mysr@gmail.com>
2024-08-22 01:31:16 +0530

    Initial Commit

---
+++ VPNSwiftUIDemo/VPNSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPNSwiftUIDemoApp.swift
+//  VPNSwiftUIDemo
+//
+//  Created by Softradix on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct VPNSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
