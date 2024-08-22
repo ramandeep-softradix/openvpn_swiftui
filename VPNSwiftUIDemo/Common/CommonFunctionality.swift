6a107c0a31900dc6e1d570a0ac737fc725d4fc0a
Dev <developer.mysr@gmail.com>
2024-08-22 20:08:37 +0530

    Open Vpn connection work is done

---
+++ VPNSwiftUIDemo/Common/CommonFunctionality.swift
@@ -0,0 +1,8 @@
+//
+//  CommonFunctionality.swift
+//  VPNSwiftUIDemo
+//
+//  Created by Softradix on 22/08/24.
+//
+
+import Foundation
---
+++ VPNSwiftUIDemo/Constants/LocalizableStrings.swift
@@ -0,0 +1,8 @@
+//
+//  LocalizableStrings.swift
+//  VPNSwiftUIDemo
+//
+//  Created by Softradix on 22/08/24.
+//
+
+import Foundation
